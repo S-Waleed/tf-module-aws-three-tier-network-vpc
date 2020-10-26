8d7e661362d480a019c5d48aa753b8e97788807e
Waleed <masterwali@Waleeds-MacBook-Pro.local>
2020-10-26 06:53:00 -0400

    type constraint and error validation on vars

--- vars.tf
+++ vars.tf
@@ -1,6 +1,20 @@
-variable ec2_iam_role_name {}
+variable ec2_iam_role_name {
+  type = string
 
-variable policy_description {}
+  validation {
+    condition     = length(var.ec2_iam_role_name) > 4 && substr(var.ec2_iam_role_name, 0, 4) == "svc-"
+    error_message = "The ec2_iam_role_name value must be a valid IAM role name, starting with \"svc-\"."
+  }
+}
+
+variable policy_description {
+  type = string
+
+  validation {
+    condition     = length(var.policy_description) > 4
+    error_message = "The policy_description value must contain more than 4 characters."
+  }
+}
 
 variable assume_role_policy {}
 
