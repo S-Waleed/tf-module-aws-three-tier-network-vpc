a9e488dfa53aa5767680ef42c6dd950c73c813fc
Waleed <masterwali0402@gmail.com>
2021-03-05 07:10:12 -0500

    routes associated

--- main.tf
+++ main.tf
@@ -13,111 +13,100 @@ resource "aws_vpc" "this" {
   )
 }
 
-# Create the route table
-resource "aws_route_table" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = merge(
-    var.additional_tags,
-    {
-      Name = "${var.vpc_name}-route-table"
-    }
-  )
+# Tag the main route table
+resource "aws_ec2_tag" "main_route_table" {
+  resource_id = aws_vpc.this.main_route_table_id
+  key         = "Name"
+  value       = "${var.vpc_name}-route-table"
 }
 
 # Create the public subnets
-resource "aws_subnet" "public_zone_a" {
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_zone_a_subnet_cidr
-  availability_zone       = "${var.aws_region}a"
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = "${var.aws_region}${var.zones[count.index]}"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.vpc_name}-public-subnet-a"
+    Name = "${var.vpc_name}-public-subnet-${var.zones[count.index]}"
   }
 }
 
-resource "aws_subnet" "public_zone_b" {
+# Create the web subnets
+resource "aws_subnet" "web" {
+  count = length(var.web_subnet_cidrs)
+
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_zone_b_subnet_cidr
-  availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
+  cidr_block              = var.web_subnet_cidrs[count.index]
+  availability_zone       = "${var.aws_region}${var.zones[count.index]}"
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.vpc_name}-public-subnet-b"
+    Name = "${var.vpc_name}-web-subnet-${var.zones[count.index]}"
   }
 }
 
-resource "aws_subnet" "web_zone_a" {
+# Create the application subnets
+resource "aws_subnet" "app" {
+  count = length(var.app_subnet_cidrs)
+
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.web_zone_a_subnet_cidr
-  availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+  cidr_block              = var.app_subnet_cidrs[count.index]
+  availability_zone       = "${var.aws_region}${var.zones[count.index]}"
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.vpc_name}-web-subnet-a"
+    Name = "${var.vpc_name}-app-subnet-${var.zones[count.index]}"
   }
 }
 
-resource "aws_subnet" "web_zone_b" {
+# Create the database subnets
+resource "aws_subnet" "data" {
+  count = length(var.data_subnet_cidrs)
+
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.web_zone_b_subnet_cidr
-  availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
+  cidr_block              = var.data_subnet_cidrs[count.index]
+  availability_zone       = "${var.aws_region}${var.zones[count.index]}"
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.vpc_name}-web-subnet-b"
-  }
-}
-
-resource "aws_subnet" "app_zone_a" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.app_zone_a_subnet_cidr
-  availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.vpc_name}-app-subnet-a"
-  }
-}
-
-resource "aws_subnet" "app_zone_b" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.app_zone_b_subnet_cidr
-  availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.vpc_name}-app-subnet-b"
-  }
-}
-
-resource "aws_subnet" "data_zone_a" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.data_zone_a_subnet_cidr
-  availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.vpc_name}-data-subnet-a"
-  }
-}
-
-resource "aws_subnet" "data_zone_b" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.data_zone_b_subnet_cidr
-  availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.vpc_name}-data-subnet-b"
+    Name = "${var.vpc_name}-data-subnet-${var.zones[count.index]}"
   }
 }
 
 # Associate the subnets with the route table
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
 
-# Set as main route table
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_vpc.this.main_route_table_id
+}
+
+resource "aws_route_table_association" "web" {
+  count = length(var.web_subnet_cidrs)
+
+  subnet_id      = element(aws_subnet.web.*.id, count.index)
+  route_table_id = aws_vpc.this.main_route_table_id
+}
+
+resource "aws_route_table_association" "app" {
+  count = length(var.app_subnet_cidrs)
+
+  subnet_id      = element(aws_subnet.app.*.id, count.index)
+  route_table_id = aws_vpc.this.main_route_table_id
+}
+
+resource "aws_route_table_association" "data" {
+  count = length(var.data_subnet_cidrs)
+
+  subnet_id      = element(aws_subnet.data.*.id, count.index)
+  route_table_id = aws_vpc.this.main_route_table_id
+}
 
 # NACLs
+# Tag the default nacl
+# Update routes on the default nacl
 
 # Security Groups
--- vars.tf
+++ vars.tf
@@ -1,55 +1,48 @@
+variable aws_region {
+  default = "us-east-1"
+}
+
+variable aws_cli_profile {
+}
+
 variable vpc_cidr {
   type = string
   default = "10.0.0.0/16"
 }
 
-variable public_zone_a_subnet_cidr {
-  type = string
-  default = "10.0.1.0/28"
-}
-
-variable public_zone_b_subnet_cidr {
-  type = string
-  default = "10.0.2.0/28"
-}
-
-variable web_zone_a_subnet_cidr {
-  type = string
-  default = "10.0.3.0/24"
-}
-
-variable web_zone_b_subnet_cidr {
-  type = string
-  default = "10.0.4.0/24"
-}
-
-variable app_zone_a_subnet_cidr {
-  type = string
-  default = "10.0.5.0/24"
-}
-
-variable app_zone_b_subnet_cidr {
-  type = string
-  default = "10.0.6.0/24"
-}
-
-variable data_zone_a_subnet_cidr {
-  type = string
-  default = "10.0.7.0/24"
-}
-
-variable data_zone_b_subnet_cidr {
-  type = string
-  default = "10.0.8.0/24"
+variable zones {
+  type = list
+  default = ["a", "b"]
 }
 
 variable vpc_name {
   type = string
+  default = "sharepoint"
 
   validation {
-    condition     = length(var.policy_description) > 4
+    condition     = length(var.vpc_name) > 4
     error_message = "The vpc_name value must contain more than 4 characters."
   }
+}
+
+variable public_subnet_cidrs {
+  type = list
+  default = ["10.0.1.0/28", "10.0.2.0/28"]
+}
+
+variable web_subnet_cidrs {
+  type = list
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable app_subnet_cidrs {
+  type = list
+  default = ["10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable data_subnet_cidrs {
+  type = list
+  default = ["10.0.7.0/24", "10.0.8.0/24"]
 }
 
 variable additional_tags {
